c1244701364ef0138ba7a8c7eb6d89789216a560
Subrat-IO <subratsethi501@gmail.com>
2025-07-19 12:23:06 +0530

    review route update

--- schema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-const review = require("./models/review");
-
-module.exports.reviewSchema = Joi.object({
-  review: Joi.object({
-    rating: Joi.number().required(),
-    comment: Joi.string().required(),  // fixed here
-  }).required()
-});
